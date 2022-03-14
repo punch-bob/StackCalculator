88c2fc5f3579c8ff59e7451f580178b75c46f803
Konstantin Rybalko <rybalkokostyan@gmail.com>
2022-03-14 11:35:50 +0700

    minor updates

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,13 +1,13 @@
 package com;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import java.util.logging.Level;
 
 import com.exception.CalculatorException;
+import com.exception.missing_data_exception.MissingDataException;
 import com.exception.undeclared_data_exceptions.UndeclaredOperation;
 import com.operations.Operation;
 
@@ -18,26 +18,18 @@ public class Main
     {
         CalculatorLogger log = new CalculatorLogger();
         
-        //choose stream
         BufferedReader in = null;
-        if(args.length > 0)
+        try 
         {
-            try
-            {
-                in = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
-            }
-            catch(IOException e)
-            {
-                log.getExceptionMessage(Level.SEVERE, e);
-                return;
-            }
-        }
-        else
+            CalculatorStream stream = new CalculatorStream();
+            in = stream.chooseStream(args);
+        } 
+        catch (FileNotFoundException e) 
         {
-            in = new BufferedReader(new InputStreamReader(System.in));
+            log.getExceptionMessage(Level.SEVERE, e);
+            return;
         }
-
-        //factory working
+        
         try
         {
             OperationFactory factory = new OperationFactory();
@@ -68,7 +60,7 @@ public class Main
                 }
             }
         }
-        catch(IOException | NullPointerException e)
+        catch(IOException | MissingDataException e)
         {
             log.getExceptionMessage(Level.SEVERE, e);
         }
