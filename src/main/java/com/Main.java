a1293d8f64ab43becf04ca40dddd58a277c25a3e
Konstantin Rybalko <rybalkokostyan@gmail.com>
2022-03-19 20:57:15 +0700

    SRP correction

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,14 +2,8 @@ package com;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import java.util.logging.Level;
-
-import com.exception.CalculatorException;
-import com.exception.missing_data_exception.MissingDataException;
-import com.exception.undeclared_data_exceptions.UndeclaredOperation;
-import com.operations.Operation;
 
 public class Main
 {
@@ -30,39 +24,8 @@ public class Main
             return;
         }
         
-        try
-        {
-            OperationFactory factory = new OperationFactory();
-            ExecutionContext executionContext = new ExecutionContext();
-            String operationLine = new String();
-            while (operationLine != null) 
-            {
-                operationLine = in.readLine();
-                try 
-                {
-                    OperationLineParser operationLineParser = new OperationLineParser();
-                    String slpitedOperationLine[] = operationLineParser.parseLine(operationLine);
-
-                    Operation operation = factory.getOperationByName(slpitedOperationLine[0]);
-                    if (operation != null)
-                    {
-                        operation.execute(executionContext, slpitedOperationLine);
-                        log.getInfoMessage(slpitedOperationLine[0]);
-                    }
-                    else
-                    {
-                        throw new UndeclaredOperation(slpitedOperationLine[0]);
-                    }
-                }
-                catch (CalculatorException | ArrayIndexOutOfBoundsException e)
-                {
-                    log.getExceptionMessage(Level.WARNING, e);
-                }
-            }
-        }
-        catch(IOException | MissingDataException e)
-        {
-            log.getExceptionMessage(Level.SEVERE, e);
-        }
+        Calculator calculator = new Calculator();
+        calculator.calculate(in);
+        
     }
 }
